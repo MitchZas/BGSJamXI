b4cad6eb70e7a9eabee6ab136a9c087a70ae884b
John8239 <94279003+John8239@users.noreply.github.com>
2025-10-25 10:48:41 -0400

    Added PauseMenuScript

---
+++ FindYourBoo/Assets/PauseMenuScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public static bool _gameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
