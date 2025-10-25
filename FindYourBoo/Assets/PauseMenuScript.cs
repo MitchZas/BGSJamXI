3d8a9799d8ca7967bd09d300144ede4608bc3c29
John8239 <94279003+John8239@users.noreply.github.com>
2025-10-25 10:56:28 -0400

    Added PauseMenuUI game object and created Pause and Resume functions to PauseMenuScript

--- FindYourBoo/Assets/PauseMenuScript.cs
+++ FindYourBoo/Assets/PauseMenuScript.cs
@@ -3,10 +3,37 @@ using UnityEngine;
 public class PauseMenuScript : MonoBehaviour
 {
     public static bool _gameIsPaused = false;
+    public GameObject _pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+        _gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        _pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+        _gameIsPaused = true;
     }
 }
