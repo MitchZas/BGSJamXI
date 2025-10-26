af6e8c05368e8f50507d1fc7496e35a0eb825d71
MitchZas <mitchellzasa@gmail.com>
2025-10-25 23:42:42 -0400

    Added a start screen!

---
+++ FindYourBoo/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void LoadOptions()
+    {
+        // TODO: Add back in when we have options
+        //SceneManager.LoadScene();
+        Debug.Log("Loading Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        //Debug.Log("Quitting Game");
+    }
+}
